05806543a708f4791fc093ecfe89bc0916027d32
Vovvka <pelyux.vla@gmail.com>
2024-05-16 00:16:19 +0300

    added gui folder

---
+++ date_converter/gui_builder/gui.py
@@ -0,0 +1,9 @@
+import tkinter
+from tkinter import ttk
+
+window = tkinter.Tk()
+
+window.title("CHG Routine")
+
+label = tkinter.Label(window, text = "CHGs routine tasks").pack()
+window.mainloop()
